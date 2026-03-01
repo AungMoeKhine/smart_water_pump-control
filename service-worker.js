0753726ce54d39c9137b6933597a94f7fe348c8f
AungMoeKhine <aungmoekhine@gmail.com>
2026-03-01 20:45:33 +0630

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'smart-pump-v1';
+const ASSETS = [
+  'cloud_control.html',
+  'logo.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
